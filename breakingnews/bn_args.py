51dd2ca9cdb03860ee139b49075b29a11088bccc
golsa-tahmasebzadeh <golsa.tahmasebzadeh@tib.eu>
2023-01-05 15:10:21 +0000

    Update bn_args.py

--- breakingnews/bn_args.py
+++ breakingnews/bn_args.py
@@ -2,10 +2,10 @@ import argparse
 
 def get_parser():
 
-    parser = argparse.ArgumentParser(description='MM-Locate-News')
-    parser.add_argument('--data_path', default='/data/1/mm-locate-news/dataset')
-    parser.add_argument('--data_to_use', default='breaking-news', type=str, help='mm-locate-news/breaking-news')
-    parser.add_argument('--results_path', default = '/data/1/mm-locate-news/breakingnews/results')
+    parser = argparse.ArgumentParser(description='BreakingNews')
+    parser.add_argument('--data_path', default='../dataset')
+    parser.add_argument('--data_to_use', default='breaking-news', type=str)
+    parser.add_argument('--results_path', default = 'breakingnews/results')
     parser.add_argument('--emb_dim', default = 1024, type=int)
     parser.add_argument('--txt_dim', default=3072, type=int)
     parser.add_argument('--img_dim', default=512, type=int)
@@ -27,10 +27,5 @@ def get_parser():
     parser.add_argument('--seed', default=1234, type=int)
     
     parser.add_argument('--model_name', default='reg_v_clip', type=str, help='reg_v_clip, reg_t_2bert, reg_mm_clip_loc_scene')
-    parser.add_argument('--check_point',
-                        default =
-    '/data/1/mm-locate-news/experiments/snapshots/breaking-news/reg_v_clip/main.epoch_20_tim.80.69_loss_4207.908.pth.tar'
-    # '/data/1/mm-locate-news/experiments/snapshots/breaking-news/reg_t_2bert/main.epoch_68_tim.80.47_loss_2942.421.pth.tar'
-    # '/data/1/mm-locate-news/experiments/snapshots/breaking-news/reg_mm_clip_loc_scene/freezeimage.epoch_49_tim.81.02_loss_2920.927.pth.tar'
-                       )
+    parser.add_argument('--check_point',  default =''  )
     return parser
