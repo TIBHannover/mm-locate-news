b2dbe7be3b7f927405c13fe64e7c379b80192c76
golsa-tahmasebzadeh <golsa.tahmasebzadeh@tib.eu>
2023-09-12 16:50:44 +0100

    Update embed_image.py

--- inference/embed_image.py
+++ inference/embed_image.py
@@ -14,14 +14,14 @@ def get_scene_feature(img_path):
     embedder = SceneEmbedding()
     im_pil = Image.open(img_path).convert('RGB')
     image_emb = embedder.embed(im_pil)
-    image_emb = image_emb.squeeze()
-    image_emb = image_emb.unsqueeze(0) #.detach().cpu().numpy()
+    image_emb = torch.from_numpy( image_emb.squeeze() )
+    image_emb = image_emb.unsqueeze(0) 
     return image_emb.to(device)
 
 def get_location_feature(img_path):
     embedder = LocationEmbedding()
     im_pil = Image.open(img_path).convert('RGB')
-    image_emb = embedder.embed(im_pil).unsqueeze(0) #.detach().cpu().numpy()
+    image_emb = torch.from_numpy( embedder.embed(im_pil).unsqueeze(0) ) 
     return image_emb.to(device)
 
 def get_obj_feature(img_path):
@@ -35,10 +35,10 @@ def get_obj_feature(img_path):
     image_tensor = preprocess(im_pil)
     image_tensor = image_tensor.unsqueeze(0)
     with torch.no_grad():
-        resnet152.eval()  # Set the model to evaluation mode
+        resnet152.eval()  
         image_emb = resnet152(image_tensor).squeeze()
     
-    image_emb = image_emb.unsqueeze(0) #.detach().cpu().numpy()
+    image_emb = image_emb.unsqueeze(0) 
     return image_emb.to(device)
 
 def get_clip_image_feature(img_path):
@@ -46,5 +46,5 @@ def get_clip_image_feature(img_path):
     model, preprocess = clip.load("ViT-B/32", device=device)
     image = preprocess(image).unsqueeze(0).to(device)
     outputs = model.encode_image(image)
-    image_emb = outputs.squeeze()      #.detach().cpu().numpy()
+    image_emb = outputs.squeeze()      
     return image_emb.unsqueeze(0)
